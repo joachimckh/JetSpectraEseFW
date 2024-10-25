f34122cdafbb322fa14ae7ca5d27fa5ef9d53c72
joachimckh <jh@joachimhansen.dk>
2024-10-25 14:45:33 +0200

    dox

--- Core/JEfficiency.cxx
+++ Core/JEfficiency.cxx
@@ -1,13 +1,8 @@
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
+/// \file JEfficiency.cxx
+/// \brief Class for unfolding and calculating efficiency
+///
+/// \author Joachim C. K. B. Hansen
 
-/*
-Author: Joachim Hansen
-*/
 
 #include "JEfficiency.h"
 
--- Core/JEfficiency.h
+++ Core/JEfficiency.h
@@ -1,14 +1,8 @@
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
+/// \file JEfficiency.h
+/// \brief Header file for JEfficiency class
+///
+/// \author Joachim C. K. B. Hansen
 
-/*
-Author: Joachim Hansen
-
-*/
 #ifndef CORE_JEfficiency_H_
 #define CORE_JEfficiency_H_
 
